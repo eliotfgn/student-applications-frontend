3f003a9f364b098e631a52e3e424a625b7650ee4
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-11 09:12:33 +0100

    feat: Add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import {HTMLProps} from "react";
+import cn from "../../utils/cn.ts";
+
+interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+export default function Button(props: ButtonProps): React.JSX.Element {
+    return (
+        <button
+            className={cn(
+                'rounded-full border border-emerald-600 bg-emerald-600 px-8 py-1 text-center font-medium text-white transition transition-colors sm:py-2 lg:text-base',
+                'hover:bg-transparent hover:text-emerald-600',
+                { 'cursor-not-allowed bg-emerald-200 text-black': props.disabled },
+                props.className,
+            )}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
+}
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import FormInput from "../../components/FormInput/FormInput.tsx";
 import {useForm} from "react-hook-form";
 import Eye from "../../components/icons/Eye.tsx";
+import Button from "../../components/Button/Button.tsx";
 
 function Login(): React.JSX.Element {
     const {register} = useForm();
 
     return (
-        <div className={'py-10 px-5 rounded-lg shadow-md flex flex-col'}>
+        <div className={'py-10 px-10 rounded-3xl shadow-md shadow-xl flex flex-col'}>
             <h2 className={'text-3xl text-center text-emerald-600 font-bold'}>Sign in</h2>
-            <div className={'mt-7'}>
+            <div className={'mt-7 w-80'}>
                 <form className={'flex flex-col gap-5'}>
                     <FormInput register={register} name={'email'} label={'Email'}/>
                     <FormInput register={register} name={'password'} label={'Password'} icon={<Eye/>} />
+
+                    <Button className={'my-5'}>Sign in</Button>
                 </form>
             </div>
         </div>
