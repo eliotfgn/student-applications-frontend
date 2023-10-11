ccba34743f17e9304daae551f05e54af7d3a7f03
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-11 07:46:36 +0100

    feat: add base auth layout content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout.tsx";
+import Login from "./pages/auth/Login.tsx";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/auth'} element={<AuthLayout/>}></Route>
+        <Route path={'/auth'} element={<AuthLayout/>}>
+          <Route path={'login'} element={<Login/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Login(): React.JSX.Element {
+    return (
+        <div></div>
+    );
+}
+
+export default Login;
