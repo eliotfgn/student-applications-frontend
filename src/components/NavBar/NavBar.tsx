02e438836a1217ea1dfca28849bf338774d3bbdd
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-11 12:59:49 +0100

    feat: add navbar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar(): React.JSX.Element {
+    return (
+        <div className={'absolute h-screen w-80 bg-emerald-600 py-10'}>
+            <h1 className={'text-2xl text-white font-bold text-center'}>Dashboard</h1>
+
+            <div>
+                <div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
+import NavBar from "../components/NavBar/NavBar.tsx";
 
 function DashboardLayout(): React.JSX.Element {
     return (
-        <div className={''}>
+        <div className={'relative'}>
+            <NavBar/>
             <div><Outlet/></div>
         </div>
     );
