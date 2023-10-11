8de47bce2af63fafa4ef89ee32ec1d1562d5bcbf
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-11 07:32:28 +0100

    feat: Add auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout.tsx";
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/auth'} element={<AuthLayout/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
