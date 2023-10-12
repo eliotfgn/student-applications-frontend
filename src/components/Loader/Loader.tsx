8e3977f2bd0af26c07176cd764a5f7349967abd2
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-12 11:19:29 +0100

    faet: fetch user applications

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Loader.css';
+import cn from '../../utils/cn.ts';
 
-function Loader(): React.JSX.Element {
-    return (
-        <div className={'loader'}></div>
-    );
+function Loader({ dark }: { dark?: boolean }): React.JSX.Element {
+    return <div className={cn('loader', dark ? 'green' : 'white')}></div>;
 }
 
 export default Loader;
--- src/pages/dashboard/applications.tsx
+++ src/pages/dashboard/applications.tsx
@@ -1,30 +1,57 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Button from '../../components/Button/Button.tsx';
 import NewApplicationModal from '../../components/Application/NewApplicationModal.tsx';
+import { useGet } from '../../hooks/api/useGet.ts';
+import { AuthContext } from '../../contexts/AuthContext.tsx';
+import Loader from '../../components/Loader/Loader.tsx';
+import Application from '../../types/models/application';
+import ApplicationItem from '../../components/Application/ApplicationItem.tsx';
 
 function Applications(): React.JSX.Element {
     const [modalOpened, setModalOpened] = useState<boolean>(false);
+    const [applications, setApplications] = useState<Application[]>([]);
+
+    const authContext = useContext(AuthContext);
+    const { isLoading, get } = useGet();
 
     const handleModal = () => {
         setModalOpened(!modalOpened);
     };
 
-    return <>
-        <header className={'w-full flex items-center justify-between'}>
-            <h3 className={'text-2xl text-emerald-600 font-medium'}>Hi, Eliot !</h3>
-            <Button onClick={handleModal}>New application</Button>
-        </header>
+    useEffect(() => {
+        get({
+            url: `/users/${authContext?.auth.user?.id.toString()}/applications`,
+            headers: { Authorization: `Bearer ${authContext?.auth.token}` },
+        }).then((response) => {
+            setApplications(response.data.data);
+        });
+    }, []);
 
-        <section className={'mt-12'}>
-            <h2 className={'text-3xl font-bold text-emerald-600'}>My Applications</h2>
+    return (
+        <>
+            <header className={'flex w-full items-center justify-between'}>
+                <h3 className={'text-2xl font-medium text-emerald-600'}>Hi, Eliot !</h3>
+                <Button onClick={handleModal}>New application</Button>
+            </header>
 
-            <div className={'my-10'}>
+            <section className={'mt-12'}>
+                <h2 className={'text-3xl font-bold text-emerald-600'}>My Applications</h2>
 
-            </div>
-        </section>
+                <div className={'my-10'}>
+                    {isLoading && <Loader dark={true} />}
+                    {applications.length < 1 ? (
+                        <p>No application yet.</p>
+                    ) : (
+                        applications.map((application) => (
+                            <ApplicationItem application={application} />
+                        ))
+                    )}
+                </div>
+            </section>
 
-        <NewApplicationModal isOpen={modalOpened} setIsOpen={setModalOpened} />
-    </>;
+            <NewApplicationModal isOpen={modalOpened} setIsOpen={setModalOpened} />
+        </>
+    );
 }
 
 export default Applications;
